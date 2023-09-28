7d4aad935af4304bebd7a7eca70d99f28a85cf57
Bernard Madureira <88686573+Wootwut@users.noreply.github.com>
2023-09-28 07:00:00 +0200

    Create one variable and one function
    Create one variable which prompts the player choice,
    and one function who display the possibilities of outcome

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ function getComputerChoice(){
 let computerSelection = getComputerChoice();
 let playerSelection = prompt("Choose your weapon!").toLowerCase();
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection){
-        return "It's a draw play again!"
-    }
 
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection ){
+        return "It's a draw! Play again !"
+    }
+    else if (playerSelection === "rock" && computerSelection == "scissors" 
+            || playerSelection == "scissors" && computerSelection == "paper"
+            || playerSelection == "paper" && computerSelection == "rock"    ){
+                return `You win, ${playerSelection} crushes ${computerSelection}`
+            }
+}   else {
+    return `You loose! ${computerSelection} beats ${playerSelection}`
 }
