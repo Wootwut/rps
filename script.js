762510841a752efce615307f1a14560da6ca0a45
Bernard Madureira <88686573+Wootwut@users.noreply.github.com>
2024-01-28 12:54:22 +0100

    create functional code didn't code for 4 months, this git is a message

--- script.js
+++ script.js
@@ -1,46 +1,81 @@
+let computerSelection = getComputerChoice();
+let playerSelection ;
+let playerPoints = 0;
+let computerPoints = 0;
+let text = document.querySelector(".text");
+let score = document.querySelector(".score")
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button =>{
+    button.addEventListener("click", () =>{
+        playerSelection = button.id;
+        computerSelection = getComputerChoice()
+        playRound(playerSelection,computerSelection)
+    })
+})
+
+
+
 function getComputerChoice(){
     const rockPaperScissors = ["rock","paper","scissors"];
     let computerWeapon = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
     return computerWeapon;
 }
 
-
-let computerSelection = getComputerChoice();
-let playerSelection = prompt("Choose your weapon!").toLowerCase();
-const draw = "It's a draw! Play again !"
-const winner = `You win, ${playerSelection} crushes ${computerSelection}`
-const looser = `You loose! ${computerSelection} beats ${playerSelection}`
-let playerPoints = 0;
-let computerPoints = 0;
-
 function playRound(playerSelection, computerSelection){
    
    
     if (playerSelection === computerSelection ){
-        return draw
+        result ="It's a draw! Play again !";
     }
     else if  (playerSelection === "rock" && computerSelection == "scissors" 
             || playerSelection == "scissors" && computerSelection == "paper"
             || playerSelection == "paper" && computerSelection == "rock"    ){
                 playerPoints ++
-                return winner;
-            }
+                result =`You win, ${playerSelection} crushes ${computerSelection}`;
+            } else {
             computerPoints++;
-    return looser;
-        }        
-function game(){
-    for (let i=0; i<4 ; i++){
-        playerSelection = prompt("Choose your weapon!").toLowerCase();
-        const rockPaperScissors = ["rock","paper","scissors"];
-         rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
-        console.log(playRound(playerSelection,computerSelection));
+    result =`You loose! ${computerSelection} beats ${playerSelection}`;
+            }
+    gameResult(result)
+   
+        }    
+
+function gameResult(result){
+    if ((computerPoints || playerPoints) < 5){
+        text.innerText = result;
+        score.innerText = "Your score: " + playerPoints + " -- Computer's score: " + computerPoints
     }
-        if (playerPoints>computerPoints){
-            return "You're the winner of this epic battle"
-        }else if (playerPoints === computerPoints){
-            return "There's no winner or looser in this epic battle"
-        } else {
-            return "You lost the war, mate"
-        }
-    }
-console.log(game())
+    else {
+       gameOver()
+    }    
+}
+function gameOver(){
+  
+   if (playerPoints > computerPoints ){
+    text.innerText = "You're the winner of this epic battle"
+   }
+   else {
+    text.innerText = "You lost the war, mate"
+   }
+   playerPoints = 0;
+   computerPoints = 0;
+   score.innerText = "Play again!"
+}
+    
+// function game(){
+//     for (let i=0; i<4 ; i++){
+//         playerSelection = prompt("Choose your weapon!").toLowerCase();
+//         const rockPaperScissors = ["rock","paper","scissors"];
+//          rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
+//         console.log(playRound(playerSelection,computerSelection));
+//     }
+//         if (playerPoints>computerPoints){
+//             return "You're the winner of this epic battle"
+//         }else if (playerPoints === computerPoints){
+//             return "There's no winner or looser in this epic battle"
+//         } else {
+//             return "You lost the war, mate"
+//         }
+//     }
+//console.log(game())
